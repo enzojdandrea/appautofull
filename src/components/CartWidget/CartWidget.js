520237ff45eef62ec5efab97a2e4b0c50faa7a15
Enzo D'Andrea <enzojdandrea@gmail.com>
2023-01-03 19:08:59 -0300

    cambiado numero de cartWidget

--- src/components/CartWidget/CartWidget.js
+++ src/components/CartWidget/CartWidget.js
@@ -1,8 +1,11 @@
 const CartWidget = () =>{
     return(
         <div class="text-light">
-            <img src="./images/shopping-cart.png" alt="cart-widget" />
-            3
+            <img src="./images/shopping-cart.png" alt="cart-widget"/>
+            <span class="position-absolute top-10 start-95 translate-middle badge rounded-pill bg-danger">
+                99+
+                <span class="visually-hidden">mensajes no leídos</span>
+            </span>
         </div>
     )
 }
