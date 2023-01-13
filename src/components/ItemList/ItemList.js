a9b2422cfaacd1528aa63e787c23ce2822f5ca06
Enzo D'Andrea <enzojdandrea@gmail.com>
2023-01-13 11:06:12 -0300

    quitados console.log inecesarios

--- src/components/ItemList/ItemList.js
+++ src/components/ItemList/ItemList.js
@@ -1,7 +1,6 @@
 import ItemCard from "../ItemCard/ItemCard"
 import "./ItemList.css"
 const ItemList = ({products}) =>{
-    console.log(products)
     return(
         <div className="row productsContainer">
             {products.map(prod=> <ItemCard key={prod.id} {...prod}/>)}
