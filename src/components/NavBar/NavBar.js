a7e0e2b9be1d7c118b97cb69114c580d80845520
Enzo D'Andrea <enzojdandrea@gmail.com>
2023-01-19 21:02:44 -0300

    cambiando navbar

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,37 +1,35 @@
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
 import { Link } from "react-router-dom"
 import CartWidget from "../CartWidget/CartWidget"
 import './NavBar.css'
 
-
-const Navbar = () => {
+function NavBarReact() {
     return (
-        <nav className="navbar navbar-expand-lg bg-dark">
-            <div className="container-fluid container">
-                <Link to='/'>
-                        <img className="logo"src="../images/default-logo.png" alt="logo-AutoFull"/>
-                </Link>                
-                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                    <span className="navbar-toggler-icon"></span>
-                </button>
-                <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                    <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-                        <li className="nav-item">
+        <Navbar bg="light" expand="lg" className="navbar navbar-expand-lg bg-dark">
+            <Container>
+                <Navbar.Brand>
+                    <Link to='/'>
+                        <img className="logo" src="../images/default-logo.png" alt="logo-AutoFull" />
+                    </Link>
+                </Navbar.Brand>
+                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Collapse id="basic-navbar-nav">
+                    <Nav className="me-auto">
+                        <Nav.Link>
                             <Link to='/category/autos'><button className="btn btn-outline-primary fs-5 m-1">Autos</button></Link>
-                        </li>
-                        <li className="nav-item">
-                            <Link to='/category/camionetas'><button className="btn btn-outline-primary fs-5 m-1">Camionetas</button></Link>
-                        </li>
-                        <li className="nav-item">
-                            <Link to='/category/motos'><button className="btn btn-outline-primary fs-5 m-1">Motos</button></Link>
-                        </li>
-                    </ul>
-                    <form className="d-flex">
-                        <CartWidget/>
-                    </form>
-                </div>
-            </div>
-        </nav>
-    )
+                        </Nav.Link>
+                        <Nav.Link><Link to='/category/camionetas'><button className="btn btn-outline-primary fs-5 m-1">Camionetas</button></Link></Nav.Link>
+                        <Nav.Link><Link to='/category/motos'><button className="btn btn-outline-primary fs-5 m-1">Motos</button></Link></Nav.Link>
+                    </Nav>
+                    <Nav.Link>
+                            <CartWidget />
+                    </Nav.Link>
+                </Navbar.Collapse>
+            </Container>
+        </Navbar>
+    );
 }
 
-export default Navbar
+export default NavBarReact;
