1f683edb78fddd0db2e5afb772594cf153f50ec0
Enzo D'Andrea <enzojdandrea@gmail.com>
2023-01-31 21:00:55 -0300

    configurando .env

--- src/services/firebase/firebaseConfig.js
+++ src/services/firebase/firebaseConfig.js
@@ -2,12 +2,12 @@ import { initializeApp } from "firebase/app";
 import {getFirestore} from "firebase/firestore"
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCiccanvxjNRfd4IZOsdYurcbnKSRdzXEM",
-  authDomain: "basedatosautofull.firebaseapp.com",
-  projectId: "basedatosautofull",
-  storageBucket: "basedatosautofull.appspot.com",
-  messagingSenderId: "1056006954245",
-  appId: "1:1056006954245:web:9b201e668d74d48b706b6c"
+  apiKey: procces.env.REACT_APP_apiKey,
+  authDomain: procces.env.REACT_APP_authDomain,
+  projectId: procces.env.REACT_APP_projectId,
+  storageBucket: procces.env.REACT_APP_storageBucket,
+  messagingSenderId: procces.env.REACT_APP_messagingSenderId,
+  appId: procces.env.REACT_APP_appId
 };
 
 const app = initializeApp(firebaseConfig);
