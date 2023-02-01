a357b5615d261055c5a70cd5be20c84c5c4417ec
Enzo D'Andrea <enzojdandrea@gmail.com>
2023-01-31 21:01:28 -0300

    cambiado firebaseconfig

--- src/services/firebase/firebaseConfig.js
+++ src/services/firebase/firebaseConfig.js
@@ -2,12 +2,12 @@ import { initializeApp } from "firebase/app";
 import {getFirestore} from "firebase/firestore"
 
 const firebaseConfig = {
-  apiKey: procces.env.REACT_APP_apiKey,
-  authDomain: procces.env.REACT_APP_authDomain,
-  projectId: procces.env.REACT_APP_projectId,
-  storageBucket: procces.env.REACT_APP_storageBucket,
-  messagingSenderId: procces.env.REACT_APP_messagingSenderId,
-  appId: procces.env.REACT_APP_appId
+  apiKey: process.env.REACT_APP_apiKey,
+  authDomain: process.env.REACT_APP_authDomain,
+  projectId: process.env.REACT_APP_projectId,
+  storageBucket: process.env.REACT_APP_storageBucket,
+  messagingSenderId: process.env.REACT_APP_messagingSenderId,
+  appId: process.env.REACT_APP_appId
 };
 
 const app = initializeApp(firebaseConfig);
